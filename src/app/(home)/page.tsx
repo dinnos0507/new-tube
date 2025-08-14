86b6da4745ed0854bf34c9cbda2d513cacfedcac
dinnos0507 <dinnos0507@gmail.com>
2025-08-15 00:17:58 +0700

    fix trpc

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,17 @@
-"use client";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Dinnos" });
 
-import { trpc } from "@/trpc/client";
-
-export default function Home() {
-  const { data } = trpc.hello.useQuery({ text: "Dinnos2" });
-  return <div>Client component says: {data?.greeting} </div>;
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
