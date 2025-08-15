1033d992a02a792755e12b5fd75c6c211c6e1ead
dinnos0507 <dinnos0507@gmail.com>
2025-08-15 18:04:00 +0700

    tRPC configure

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,8 +1,11 @@
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { users } from "@/db/schema";
 import { auth } from "@clerk/nextjs/server";
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { cache } from "react";
 import superjson from "superjson";
-
+import { ratelimit } from "@/lib/ratelimit";
 export const createTRPCContext = cache(async () => {
   const { userId } = await auth();
 
@@ -24,3 +27,35 @@ const t = initTRPC.context<Context>().create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+export const protectedProcedure = t.procedure.use(async function isAuthed(
+  opts
+) {
+  const { ctx } = opts;
+  if (!ctx.clerkUserId) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.clerkId, ctx.clerkUserId))
+    .limit(1);
+
+  if (!user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  const { success } = await ratelimit.limit(user.id);
+
+  if (!success) {
+    throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+  }
+
+  return opts.next({
+    ctx: {
+      ...ctx,
+      user,
+    },
+  });
+});
